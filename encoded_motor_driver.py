6b6dcd9a6c2ae36802b5027bf59452e2f7d0c6fb
starmarens <phillips85502@gmail.com>
2025-12-09 21:01:53 -0600

    finished coding

---
+++ encoded_motor_driver.py
@@ -0,0 +1,97 @@
+from motor_driver import MotorDriver
+from machine import Pin
+
+
+class EncodedMotorDriver(MotorDriver):
+    def __init__(self, driver_ids: list | tuple, encoder_ids: list | tuple) -> None:
+        super().__init__(*driver_ids)
+        # Pin configuration
+        self.enc_a_pin = Pin(encoder_ids[0], Pin.IN)
+        self.enc_b_pin = Pin(encoder_ids[1], Pin.IN)
+        self.enc_a_pin.irq(
+            trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self.update_counts_a
+        )
+        self.enc_b_pin.irq(
+            trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self.update_counts_b
+        )
+        # Variables
+        self.enc_a_val = self.enc_a_pin.value()
+        self.enc_b_val = self.enc_b_pin.value()
+        self.encoder_counts = 0
+        self.prev_counts = 0
+        self.meas_ang_vel = 0.0
+        self.meas_lin_vel = 0.0
+
+    def update_counts_a(self, pin):
+        self.enc_a_val = pin.value()
+        if self.enc_a_val == 1:
+            if self.enc_b_val == 0:  # a=1, b=0
+                self.encoder_counts += 1
+            else:  # a=1, b=1
+                self.encoder_counts -= 1
+        else:
+            if self.enc_b_val == 0:  # a=0, b=0
+                self.encoder_counts -= 1
+            else:  # a=0, b=1
+                self.encoder_counts += 1
+
+    def update_counts_b(self, pin):
+        self.enc_b_val = pin.value()
+        if self.enc_b_val == 1:
+            if self.enc_a_val == 0:  # b=1, a=0
+                self.encoder_counts -= 1
+            else:  # b=1, a=1
+                self.encoder_counts += 1
+        else:
+            if self.enc_a_val == 0:  # b=0, a=0
+                self.encoder_counts += 1
+            else:  # b=0, a=1
+                self.encoder_counts -= 1
+
+    def reset_encoder_counts(self):
+        self.encoder_counts = 0
+
+
+# TEST
+if __name__ == "__main__":  # Test only the encoder part
+    from utime import sleep
+
+    # SETUP
+    # emd = EncodedMotorDriver(
+    #     driver_ids=(9, 11, 10),
+    #     encoder_ids=(16, 17),
+    # )  # right motor, encoder's green and yellow on GP19 and GP20
+    emd = EncodedMotorDriver(
+        driver_ids=(15, 13, 14),
+        encoder_ids=(10, 11),
+    )  # left motor, encoder's green and yellow on GP18 and GP19
+    STBY = Pin(12, Pin.OUT)
+    STBY.off()
+
+    # LOOP
+    STBY.on()  # enable motor driver
+    # Forwardly ramp up and down
+    for i in range(100):
+        emd.forward((i + 1) / 100)
+        print(f"f, dc: {i}%, enc_cnt: {emd.encoder_counts}")
+        sleep(4 / 100)  # 4 seconds to ramp up
+    for i in reversed(range(100)):
+        emd.forward((i + 1) / 100)
+        print(f"f, dc: {i}%, enc_cnt: {emd.encoder_counts}")
+        sleep(4 / 100)  # 4 seconds to ramp down
+    # Backwardly ramp up and down
+    for i in range(100):
+        emd.backward((i + 1) / 100)
+        print(f"f, dc: {i}%, enc_cnt: {emd.encoder_counts}")
+        sleep(4 / 100)  # 4 seconds to ramp up
+    for i in reversed(range(100)):
+        emd.backward((i + 1) / 100)
+        print(f"f, dc: {i}%, enc_cnt: {emd.encoder_counts}")
+        sleep(4 / 100)  # 4 seconds to ramp down
+
+    # Terminate
+    emd.stop()
+    sleep(0.5)
+    print("motor stopped.")
+    STBY.off()  # disable motor driver
+    print("motor driver disabled.")
--- trail_following.py
+++ trail_following.py
@@ -1,7 +1,125 @@
 from encoded_motor_driver import EncodedMotorDriver
 from time import sleep
+from machine import Pin
 
 # SETUP
-
+left_motor = EncodedMotorDriver((15,13,14), (10, 11))
+right_motor = EncodedMotorDriver((16,18,17), (19, 20))
+stby = Pin(12, Pin.OUT)
+stby.on()
 
 # LOOP
+
+start = True
+Checkpoint_1 = False
+Checkpoint_2 = False
+Checkpoint_3 = False
+Checkpoint_4 = False
+Finish = False
+FinishPart2 = False
+right_motor_distance = 0
+left_motor_distance = 0
+
+#axel distance is 134 mm
+#center point is at 134 / 2 = 67 mm
+
+
+def motor_in_range(distance, current_distance):
+    
+    inRange = current_distance - 15 < distance < current_distance + 15
+    return inRange
+    
+    
+
+while start == True:
+    right_motor_distance = (right_motor.encoder_counts / (98.5 * 28)) * 168
+    left_motor_distance = (left_motor.encoder_counts/ (98.5 * 28)) * 168
+    print(f"left_distance = {left_motor_distance}, right_distance = {right_motor_distance}")
+    if motor_in_range(750, right_motor_distance) and motor_in_range(750, left_motor_distance):
+        start = False
+        Checkpoint_1 = True
+        right_motor.stop() 
+        left_motor.stop()
+        sleep(3)
+        break
+    right_motor.forward(.25)
+    left_motor.forward(.265)
+    
+while Checkpoint_1 == True:
+    right_motor_distance = (right_motor.encoder_counts / (98.5 * 28)) * 168
+    left_motor_distance = (left_motor.encoder_counts/ (98.5 * 28)) * 168
+    print(f"left_distance = {left_motor_distance}, right_distance = {right_motor_distance}")
+    if motor_in_range(750 + 105, right_motor_distance) and motor_in_range(750 - 105, left_motor_distance):
+         Checkpoint_1 = False
+         Checkpoint_2 = True 
+         right_motor.stop() 
+         left_motor.stop()
+         sleep(1)
+         break
+    right_motor.forward(.25)
+    left_motor.backward(.265)
+while Checkpoint_2 == True:
+    right_motor_distance = (right_motor.encoder_counts / (98.5 * 28)) * 168
+    left_motor_distance = (left_motor.encoder_counts/ (98.5 * 28)) * 168
+    print(f"left_distance = {left_motor_distance}, right_distance = {right_motor_distance}")
+    if motor_in_range(750 + 105 + 500, right_motor_distance) and motor_in_range(750 - 105 + 500, left_motor_distance):
+        Checkpoint_2 = False
+        Checkpoint_3 = True
+        right_motor.stop() 
+        left_motor.stop()
+        sleep(3)
+        break
+    right_motor.forward(.25)
+    left_motor.forward(.265)
+while Checkpoint_3 == True:
+    right_motor_distance = (right_motor.encoder_counts / (98.5 * 28)) * 168
+    left_motor_distance = (left_motor.encoder_counts/ (98.5 * 28)) * 168
+    print(f"left_distance = {left_motor_distance}, right_distance = {right_motor_distance}")
+    if motor_in_range(750 + 105 + 500 - 316, right_motor_distance) and motor_in_range(750 - 105 + 500 + 316, left_motor_distance):
+        Checkpoint_3 = False
+        Checkpoint_4 = True
+        right_motor.stop() 
+        left_motor.stop()
+        sleep(1)
+        break
+    right_motor.backward(.25)
+    left_motor.forward(.265)
+while Checkpoint_4 == True:
+    right_motor_distance = (right_motor.encoder_counts / (98.5 * 28)) * 168
+    left_motor_distance = (left_motor.encoder_counts/ (98.5 * 28)) * 168
+    print(f"left_distance = {left_motor_distance}, right_distance = {right_motor_distance}")
+    if motor_in_range(750 + 105 + 500 - 316 + 500, right_motor_distance) and motor_in_range(750 - 105 + 500 + 316 + 500, left_motor_distance):
+        Checkpoint_4 = False
+        Finish = True
+        right_motor.stop() 
+        left_motor.stop()
+        sleep(3)
+        break
+    right_motor.forward(.25)
+    left_motor.forward(.265)
+while Finish == True:
+    right_motor_distance = (right_motor.encoder_counts / (98.5 * 28)) * 168
+    left_motor_distance = (left_motor.encoder_counts/ (98.5 * 28)) * 168
+    print(f"left_distance = {left_motor_distance}, right_distance = {right_motor_distance}")
+    if motor_in_range( 750 + 105 + 500 - 316 + 500 + 78, right_motor_distance) and motor_in_range(750 - 105 + 500 + 316 + 500 - 72, left_motor_distance):
+        Finish = False
+        FinishPart2 = True
+        right_motor.stop() 
+        left_motor.stop()
+        sleep(1)
+        break
+    right_motor.forward(.25)
+    left_motor.backward(.265)
+while FinishPart2 == True:
+    right_motor_distance = (right_motor.encoder_counts / (98.5 * 28)) * 168
+    left_motor_distance = (left_motor.encoder_counts/ (98.5 * 28)) * 168
+    print(f"left_distance = {left_motor_distance}, right_distance = {right_motor_distance}")
+    if motor_in_range( 750 + 105 + 500 - 316 + 500 + 78 + 559, right_motor_distance) and motor_in_range(750 - 105 + 500 + 316 + 500 - 78 + 559, left_motor_distance):
+        right_motor.stop()
+        left_motor.stop()
+        right_motor.stop() 
+        left_motor.stop()
+        sleep(3)
+        break
+    right_motor.forward(.25)
+    left_motor.forward(.265)
